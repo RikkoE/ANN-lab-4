24f5f900e695b62744f3ad6d298a293a67580daa
Rikko <rickard.e.1992@gmail.com>
2016-02-23 09:51:30 +0100

    Working test code
    The test code can now interpret all the given patterns and fix 2-bit
    distortion.

--- test4.m
+++ test4.m
@@ -5,11 +5,26 @@ function [ output_args ] = test4( input_args )
     x3 = vm([0 1 1 0 0 1 0 1]);
     
     x1d = vm([1 0 1 0 1 0 0 1]);
+    x2d = vm([1 1 0 0 0 1 0 0]);
+    x3d = vm([1 1 1 0 1 1 0 1]);
 
-    W = x1' * x1;
-    %SUG MIN S?CK%
+    W = x1' * x1 + x2' * x2 + x3' * x3;
+        
+    disp('Weight matrix');
     disp(W);
-    new = t0(sgn(W*x1d')');
-    disp(new);
+    
+    disp('Original');
+    disp(t0(x3));
+    
+    disp('Distorted');
+    disp(t0(x3d));
+    
+    disp('After correction');
+    new = sgn(W*x3d')';
+    disp(t0(new));
+    
+    disp('Second iteration');
+    new = sgn(W*new')';
+    disp(t0(new));
 end
 
