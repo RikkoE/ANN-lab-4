4b64cc043a76e18f61f454c9b4dd53fa8a6d199a
Rikko <rickard.e.1992@gmail.com>
2016-02-23 20:56:33 +0100

    further develop sequpdate

--- sequpdate.m
+++ sequpdate.m
@@ -4,25 +4,39 @@ function [ output_args ] = sequpdate( input_args )
 pict;
 w = p1'*p1 + p2'*p2 + p3'*p3;
 
-iter = 20000;
+iter = 10000;
 subiter = 0;
 unit = randi([1,1024], iter, 1);
-disp(unit);
 
-%     vis(p22);
-% vis(sgn(w * p11')');
+% disp('P11 energy before fix');
+% disp(-(sum(sum(w*p11'*p11))));
+% 
+% p11 = sgn(w*p11')';
+% 
+% disp('P11 energy after fix');
+% disp(-(sum(sum(w*p11'*p11))));
 
-% disp(p11');
+energyvect = ones(10,1);
 
 for i = 1:iter
-    pattern = sgn(w(unit(i),:) * p22');
-    p22(1,unit(i)) = pattern;
+    pattern = sgn(w(unit(i),:) * p11');
+    p11(1,unit(i)) = pattern;
     subiter = subiter + 1;
-    if subiter == 100
-        imagesc(reshape(p22,32,32));
+    if subiter == 1000
+        energyvect(i/1000, 1) = -(sum(sum(w*p11'*p11)));
         subiter = 0;
     end
 end
+
+% vis(p11);
+
+x = [1 2 3 4 5 6 7 8 9 10];
+
+plot(x, energyvect);
+
+% 
+% disp('P22 energy after fix');
+% disp(-(sum(sum(w*p22'*p22))));
 
 end
 
