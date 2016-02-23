6a6164dcd5200783652dd55d04454cbe977a9576
typsadde <shemani@kth.se>
2016-02-23 09:25:29 +0100

    test change
    juan kan suga

--- test4.m
+++ test4.m
@@ -7,7 +7,7 @@ function [ output_args ] = test4( input_args )
     x1d = vm([1 0 1 0 1 0 0 1]);
 
     W = x1' * x1;
-    
+    %juan kan suga massiv pung%
     disp(W);
     new = t0(sgn(W*x1d')');
     disp(new);
